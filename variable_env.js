c7ac616ef38a55990b243fcc845a23a3fe79100d
Bhanu Kiran <bhanukirant99@gmail.com>
2022-01-12 04:08:02 +0530

    file: adding variable_env.js

---
+++ variable_env.js
@@ -0,0 +1,15 @@
+
+var a = 5;
+getNum1();
+getNum2();
+console.log(a);
+
+function getNum1() {
+    var a = 15;
+    console.log(a);
+}
+
+function getNum2() {
+    var a = 50;
+    console.log(a);
+}
